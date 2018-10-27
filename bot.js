b002fa9b6470f094d3a791c9cc1651f23ea5904c
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-10-27 09:27:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setActivity("",{type: 'watching'})
+ client.user.setActivity(".author.avatarURL")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
