fdbd666f94fa4cad9ad6ae4ae00ed15005768c69
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-10-27 02:24:40 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -494,6 +494,28 @@ if (message.content.startsWith(prefix + 'trans')) {
     }
 }
 });
+////////////////
+client.on('message' , async (message) => {
+       if(message.content.startsWith(`<@${client.user.id}>`)) {
+              message.channel.startTyping()
+ let responses = [
+        'تحتاج مساعدة جرب g!help',
+        'هلا امرني',
+        'تحبني؟',
+         'طفشان عندك نكتة؟',
+         'تمنشن بوت؟',
+         'اعجبك البوت',
+         'لا تنسى تزور سيرفر السبورت',
+       'احبك ياخي'
+    ]
+    
+    // Fetch a random item from the array
+    let fetched = responses[Math.floor(Math.random() * responses.length)];
+   message.reply(fetched)
+   message.channel.stopTyping()
+       }
+  
+});
 
 
 
