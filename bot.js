2d0081804a429f7c5033b65bc937f113621fd7e4
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-10-27 03:26:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const tpoints = {};
 const vpoints = {};
 const db = require('quick.db');
-const client = new Discord.Client();   
 const giphy = require('giphy-api')();    
 const googl = require('goo.gl');  
 const translate = require('google-translate-api');   
