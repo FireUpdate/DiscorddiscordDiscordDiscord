bf656b7b72ee48089e1e4bd656497496adb856d0
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-10-27 03:34:55 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -503,6 +503,21 @@ client.on('message' , async (message) => {
 });
 
 //////////////////////////////////////////
+ client.on('message', message =>{
+
+    if(message.content == "g!roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
  
 
 
