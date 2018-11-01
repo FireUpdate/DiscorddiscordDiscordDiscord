e804dc9f0a9b09817f9abadd668a93ef3b0b87c9
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-01 16:22:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -248,30 +248,9 @@ const channel = sWlc[message.guild.id].channel
  
 
 
-const sWlc = {}
-const premium = ['411137717884289024']
-client.on('message', message => {
-var prefix = "g!";
-if(message.channel.type === "dm") return;
-if(message.author.bot) return;
-  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
-    channel: "welcome"
-}
-const channel = sWlc[message.guild.id].channel
-  if (message.content.startsWith(prefix + "setwelcomer")) {
-    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
-    let newChannel = message.content.split(' ').slice(1).join(" ")
-    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
-    sWlc[message.guild.id].channel = newChannel
-    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
-  }
-});
- 
-
-
 client.on("guildMemberAdd", member => {
       if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
-    channel: "»welcome"
+    channel: "welcome"
   }
   const channel = sWlc[member.guild.id].channel
     const sChannel = sWlc[member.guild.id].channel
