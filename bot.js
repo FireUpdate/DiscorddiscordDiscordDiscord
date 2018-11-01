721fb3f567e2a68c778cfad053c1c3f77bad16f5
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-01 16:25:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -250,7 +250,7 @@ const channel = sWlc[message.guild.id].channel
 
 client.on("guildMemberAdd", member => {
       if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
-    channel: "welcome"
+    channel: "»welcome"
   }
   const channel = sWlc[member.guild.id].channel
     const sChannel = sWlc[member.guild.id].channel
