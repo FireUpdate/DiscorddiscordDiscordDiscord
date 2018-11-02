942a43db9e890772032f9db34dec37a650aa9ac6
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-02 16:18:03 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ var Canvas = require('canvas')
 var jimp = require('jimp')
 const config = require("./config.json")
 const pretty = require('pretty-ms');
-const prefix = "+";
+const prefix = "g!";
 var guilds = {};
 
 client.on('ready', () => {
@@ -836,7 +836,46 @@ message.channel.sendFile(canvas.toBuffer())
 //┃┃╭━┫╰━╯┃┃┃┃┃┃╭━━┻━━╮┃
 //┃╰┻━┃╭━╮┃┃┃┃┃┃╰━━┫╰━╯┃
 //╰━━━┻╯╱╰┻╯╰╯╰┻━━━┻━━━╯
+const discord = require ('discord.js');
+const Jimp = require(`jimp`)
 
+if (message.content.startsWith(`g!ship`)) {
+        let replies = [`Will Never Work`, `Slight Chance of Working`, `👀`, `Can Work`, `Could Work`, `High Chance of Working`, `Will Work <3`, `A Great Match <3`, `PERFECT MATCH ❤`]
+        let result = Math.floor(Math.random() * replies.length);
+        let mUser = message.mentions.users.first()
+        let mUser2 = message.mentions.users.last()
+        if(!mUser) return message.channel.send(`Please specifify two users next time!`)
+        const mUserA = mUser.avatarURL
+        var imagetobase = `https://cdn.discordapp.com/attachments/470109023807602699/471821113261817866/Shipping.png`;
+        Jimp.read(mUser.avatarURL, function (err, imagetouse) {
+            if (err) throw err;
+            imagetouse.quality(60)
+                      .resize(866, 866)
+                      .write("imagetouse.jpg");
+            Jimp.read(mUser2.avatarURL, function (err, imagetouse2) {
+            if (err) throw err;
+           
+ imagetouse2.quality(60)
+                      .resize(866, 866)
+                      .write("imagetouse2.jpg");
+            Jimp.read(imagetobase, function (err, mydude) {
+                if (err) throw err;
+                Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function (font) {
+                    mydude.quality(60)
+                    mydude.print(font, 635, 1717, `${message.author.username}`)
+                    mydude.composite( imagetouse, 784, 797 )
+                    mydude.composite( imagetouse2, 2191, 797)
+                    mydude.write("saveme.jpg")
+                    mydude.getBuffer(`image/jpeg`, (err, buf) => {
+                        if (err) return err;
+                        message.channel.send({files: [{attachment: buf, name: `saveme.jpg`}] })
+                        message.channel.send(replies[result])
+                        message.channel.send(`MADE BY COLONIAL#1923`)
+                    })
+                })
+            })}
+        )})
+    }
 
             
 client.login(process.env.BOT_TOKEN);
