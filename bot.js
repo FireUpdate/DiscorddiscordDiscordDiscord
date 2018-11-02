1f83557b8eee20ac7d37b9e265c7ae9a63f7d786
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-02 02:40:02 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js");
+const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const Canvas = require("canvas");
-const jimp = require("jimp");
+const dateFormat = require('dateformat');
+const convert = require("hh-mm-ss")
+const fs = require('fs');
+const adminprefix = "g!";
+const devs = ['411137717884289024', '470602554611597315'];
 const moment = require('moment');
+const ytdl = require('ytdl-core');
 const yt = require('ytdl-core');
-const prefix = 'g!'
+const request = require('request');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+var Canvas = require('canvas')
+var jimp = require('jimp')
+const config = require("./config.json")
+const pretty = require('pretty-ms');
+const prefix = "+";
+var guilds = {};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
