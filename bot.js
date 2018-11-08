e17989b335eb9f8cd5d3aeacd591dad992f1956b
Botmusic1 <44499263+Botmusic1@users.noreply.github.com>
2018-11-08 15:26:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1372,8 +1372,12 @@ message.channel.send('**لديك 15 ثانيه**').then(msg => {
 
 
 
-
-
+client.on("ready", () => {
+let channel =     client.channels.get("492464188224962572")
+setInterval(function() {
+channel.send(الله اكبر);
+}, 25)
+})
 
 
 
